d68acfe38644340a4bd1ceb9358c6b801c625ccd
Abhishek <abhishekthalatoty@gmail.com>
2020-08-23 18:04:12 +0530

    config: add DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './modules/api/api.module';
 import { WebModule } from './modules/web/web.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './environment';
 
 @Module({
-  imports: [ApiModule, WebModule],
+  imports: [ApiModule, WebModule, MongooseModule.forRoot(environment.DB_PATH)],
 })
 export class AppModule {}
--- src/environment.ts
+++ src/environment.ts
@@ -6,4 +6,5 @@ export const environment = {
   WEB_HOST: process.env.WEB_HOST,
   API_HOST: process.env.API_HOST,
   PORT: process.env.PORT,
+  DB_PATH: process.env.DB_PATH,
 };
