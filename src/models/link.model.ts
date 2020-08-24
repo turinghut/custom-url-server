81e2a017f336153b656bf1309ed20ae0b2acff8a
Abhishek <abhishekthalatoty@gmail.com>
2020-08-24 10:40:28 +0530

    fix: remove classes for models

--- src/models/link.model.ts
+++ src/models/link.model.ts
@@ -1,21 +1,10 @@
 export enum LinkStatus {
-  inactive = 0,
+  inactive,
   active,
   deleted,
 }
 
 export interface ILink {
-  _id: string;
-  name: string;
-  userId: string;
-  createdAt: Date;
-  status: LinkStatus;
-  customUrl: string;
-  redirectsTo: string;
-  inPool: boolean;
-}
-
-export class Link implements ILink {
   _id: string;
   name: string;
   userId: string;
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,11 +5,3 @@ export interface IUser {
   emailAddress: string;
   phoneNumber: string;
 }
-
-export class User implements IUser {
-  _id: string;
-  name: string;
-  joinedAt: Date;
-  emailAddress: string;
-  phoneNumber: string;
-}
