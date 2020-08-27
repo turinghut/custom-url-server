8d928bdbcd40e985a7c9fff61712f757ac18ec62
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-08-27 19:28:41 +0530

    feat : implement users/:id endpoint

--- src/modules/api/user/user.controller.ts
+++ src/modules/api/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from 'src/schemas/user.schema';
 
-@Controller('users/:id')
+@Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserData(@Param() params) {
+    const user = this.userService.getUserData(params.id);
+    return user;
+  }
 }
