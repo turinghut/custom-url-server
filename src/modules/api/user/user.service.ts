0f15a72a6bfb968152e0282544ced25b98487a3b
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-08-29 18:45:07 +0530

    fix : removed getAllUsers , changed response format

--- src/modules/api/user/user.service.ts
+++ src/modules/api/user/user.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
+import { async } from 'rxjs';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  getUserData(id) {
-    return this.userModel.findById(id);
-  }
-
-  getAllUsers() {
-    return this.userModel.find({});
+  async getUserById(id) {
+    return await this.userModel.findById(id);
   }
 }
