1f6777fbaa5568fe1e1aabc25a4601389fc4da19
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-09-15 13:14:52 +0530

    add : class validators

--- src/modules/api/user/user.dto.ts
+++ src/modules/api/user/user.dto.ts
@@ -1,7 +1,18 @@
-export interface UserDTO {
+import { IsNotEmpty, IsString } from 'class-validator';
+export class UserDTO {
+  @IsNotEmpty()
+  @IsString()
   _id: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   joinedAt: Date;
+
+  @IsString()
   emailAddress: string;
+
+  @IsString()
   phoneNumber: string;
 }
