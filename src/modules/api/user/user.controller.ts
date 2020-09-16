d879788e65495a0d8045d5d1bf20240a395c14a9
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-09-16 15:24:59 +0530

    feat : add edit userdata end point

--- src/modules/api/user/user.controller.ts
+++ src/modules/api/user/user.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Put } from '@nestjs/common';
+import { IResult } from 'src/common/interfaces/response';
+import { IUser } from 'src/models/user.model';
+import { User } from 'src/schemas/user.schema';
+import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
 
-@Controller('users/:id')
+@Controller('users')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') userId: string,
+    @Body() userDTO: UserDTO,
+  ): Promise<IResult<UserDTO>> {
+    try {
+      const updatedUser = await this.userService.update(userId, userDTO);
+      if (updatedUser) {
+        return {
+          status: 'OK',
+          result: {
+            _id: updatedUser._id,
+            emailAddress: updatedUser.emailAddress,
+            phoneNumber: updatedUser.phoneNumber,
+            joinedAt: updatedUser.joinedAt,
+            name: updatedUser.name,
+          } as UserDTO,
+        };
+      }
+      return {
+        status: 'NOT OK',
+        error: 'An error occured',
+      } as IResult<UserDTO>;
+    } catch (error) {
+      return {
+        status: 'NOT OK',
+        error: error.message,
+      } as IResult<UserDTO>;
+    }
+  }
 }
--- src/modules/api/user/user.service.ts
+++ src/modules/api/user/user.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
+import { IUser } from 'src/models/user.model';
+import { UserDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async update(userId: string, user: User): Promise<IUser> {
+    const updatedUser = await this.userModel.findByIdAndUpdate(userId, user);
+    return updatedUser as IUser;
+  }
 }
