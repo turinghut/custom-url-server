c49dd0002a299334bca917311ed4afac417a9899
Sindhuja Ponnuru <sindhuja.ponnuru@gmail.com>
2020-09-16 12:34:53 +0530

    fix : service returns IUser, controller manually generates UserDTO

--- src/modules/api/user/user.service.ts
+++ src/modules/api/user/user.service.ts
@@ -3,13 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
 import { UserDTO } from './user.dto';
+import { IUser } from 'src/models/user.model';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async getUserById(id: string): Promise<UserDTO> {
+  async getUserById(id: string): Promise<IUser> {
     const user = await this.userModel.findById(id);
-    return user as UserDTO;
+    return user as IUser;
   }
 }
